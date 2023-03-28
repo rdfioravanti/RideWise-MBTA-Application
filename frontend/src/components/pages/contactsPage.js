81a1caecdd3b3bc46d51e2e4f535651fdb6c8840
MikeSabella <s0351517@salemstate.edu>
2023-03-28 09:59:41 -0400

    contacts page info updated

---
+++ frontend/src/components/pages/contactsPage.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const ContactList = () => {
+  const [contacts, setContacts] = useState([
+    { id: 1, name: 'Information & Support', phone: '617-222-3200' },
+    { id: 2, name: 'Emergency Contacts:  Transit Police', phone: '617-222-1212' },
+    { id: 3, name: 'Report a railroad crossing gate issue', phone: '800-522-8236' },
+  ]);
+
+  return (
+    <div>
+      <h1>Contact List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>{contact.name}</td>
+              <td>{contact.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ContactList;
