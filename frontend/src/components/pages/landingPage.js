f9c2347d7d4ec76d0b168e50f3141ec6b1b7c4c2
MikeSabella <s0351517@salemstate.edu>
2023-03-28 08:55:32 -0400

    updated home page

--- frontend/src/components/pages/landingPage.js
+++ frontend/src/components/pages/landingPage.js
@@ -10,7 +10,7 @@ const Landingpage = () => {
         <Card style={{ width: '30rem' }} className="mx-2 my-2">
         <Card.Body>
           <Card.Title>RideWise MBTA App</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">This is the landing page for our application that allows users to view a live feed of MBTA as well as routes, alerts,fares and contact.</Card.Subtitle>
+          <Card.Subtitle className="mb-2 text-muted">This is the landing page for our application that allows users to view a live feed of MBTA as well as routes, alerts, fares and contact.</Card.Subtitle>
           <Card.Text>
           </Card.Text>
           <Card.Link href="/signup">Sign Up</Card.Link>
