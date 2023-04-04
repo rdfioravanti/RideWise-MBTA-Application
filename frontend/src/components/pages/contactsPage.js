3dfc2767eb41d05034035a669471c803fc0ff9d7
MikeSabella <s0351517@salemstate.edu>
2023-04-04 10:33:06 -0400

    added jpg's to be used

--- frontend/src/components/pages/contactsPage.js
+++ frontend/src/components/pages/contactsPage.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+
 const ContactList = () => {
   return (
     <Container fluid>
