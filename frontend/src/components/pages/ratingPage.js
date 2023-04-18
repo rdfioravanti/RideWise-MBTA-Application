2742ed565451a195c60efc0256c70b9695b93e6f
MikeSabella <s0351517@salemstate.edu>
2023-04-18 10:34:30 -0400

    display previous comments on ratingPage

--- frontend/src/components/pages/ratingPage.js
+++ frontend/src/components/pages/ratingPage.js
@@ -1,81 +1,119 @@
-import React, { useState } from "react";
-import { Button, Form, Modal } from "react-bootstrap";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ListGroup, ListGroupItem, Form, Button, Modal, Row, Col, Container } from 'react-bootstrap';
 
-
-function CommentForm() {
-  const [username, setUsername] = useState("");
-  const [rating, setRating] = useState("");
-  const [comment, setComment] = useState("");
+const RatingList = () => {
+  const [ratings, setRatings] = useState([]);
+  const [username, setUsername] = useState('');
+  const [rating, setRating] = useState('');
+  const [comment, setComment] = useState('');
   const [showModal, setShowModal] = useState(false);
-  const [modalMessage, setModalMessage] = useState("");
+  const [modalMessage, setModalMessage] = useState('');
 
+  useEffect(() => {
+    const fetchRatings = async () => {
+      const result = await axios.get('http://localhost:8081/rating/getAll');
+      setRatings(result.data);
+    };
+    fetchRatings();
+  }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const response = await fetch("http://localhost:8081/rating/add", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        username: username,
-        rating: rating,
-        comment: comment
-      })
+    const response = await axios.post('http://localhost:8081/rating/add', {
+      username: username,
+      rating: rating,
+      comment: comment,
     });
-   
-    if (response.ok) {
+    if (response.status === 200) {
+      setModalMessage('Rating added successfully!');
       setShowModal(true);
-      setModalMessage("Comment added successfully!");
-      setUsername("");
-      setRating("");
-      setComment("");
+      setUsername('');
+      setRating('');
+      setComment('');
+      const result = await axios.get('http://localhost:8081/rating/getAll');
+      setRatings(result.data);
+    } else {
+      setModalMessage('Error adding rating');
+      setShowModal(true);
     }
   };
 
-
   const handleCloseModal = () => setShowModal(false);
 
-
   return (
-    <>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter username" value={username} onChange={(e) => setUsername(e.target.value)} required />
-        </Form.Group>
-
-
-        <Form.Group controlId="formRating">
-          <Form.Label>Rating</Form.Label>
-          <Form.Control type="text" placeholder="Enter rating" value={rating} onChange={(e) => setRating(e.target.value)} required />
-        </Form.Group>
-
-
-        <Form.Group controlId="formComment">
-          <Form.Label>Comment</Form.Label>
-          <Form.Control as="textarea" rows={3} placeholder="Enter comment" value={comment} onChange={(e) => setComment(e.target.value)} required />
-        </Form.Group>
-
-
-        <Button variant="primary" type="submit">Submit</Button>
-      </Form>
-
-
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Comment Added</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{modalMessage}</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <Container>
+      <Row>
+        <Col xs={12} md={6}>
+          <h2>Add Rating</h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </Form.Group>
+  
+            <Form.Group controlId="formRating">
+              <Form.Label>Rating</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter rating"
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+                required
+              />
+            </Form.Group>
+  
+            <Form.Group controlId="formComment">
+              <Form.Label>Comment</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="Enter comment"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                required
+              />
+            </Form.Group>
+  
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+  
+          <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>Add Rating</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{modalMessage}</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleCloseModal}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Col>
+  
+        <Col xs={12} md={6}>
+          <h2>Previous Ratings</h2>
+          <ListGroup>
+            {ratings.map((rating) => (
+              <ListGroupItem key={rating.id}>
+                <h5>{rating.username}</h5>
+                <p>Rating: {rating.rating}</p>
+                <p>{rating.comment}</p>
+              </ListGroupItem>
+            ))}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
-
-export default CommentForm;
-
-
+export default RatingList;
